9daf6e80fd850a050742aaa370c386793bae6314
Forbes Lindesay <forbes@lindesay.co.uk>
2019-11-10 16:01:10 +0000

    test: deployment via CI

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import http from 'http';
 
 http
   .createServer((_req, res) => {
-    res.end('Hello dokku-ci-user');
+    res.end('Hello dokku-ci-user 2');
   })
   .listen(process.env.PORT || 3000);
