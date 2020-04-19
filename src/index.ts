b6454de48e68181d9901c29cae45650ec2a93174
Forbes Lindesay <forbes@lindesay.co.uk>
2020-04-19 16:42:44 +0100

    feat: sample json logging

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import http from 'http';
 
 http
-  .createServer((_req, res) => {
+  .createServer((req, res) => {
+    console.info(
+      JSON.stringify({
+        method: req.method,
+        url: req.url,
+        status: 200,
+        duration: Math.floor(Math.random() * 100),
+      }),
+    );
     res.end(process.env.ENV_VAR);
   })
   .listen(process.env.PORT || 3000);
